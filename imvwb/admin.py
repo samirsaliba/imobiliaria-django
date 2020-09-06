2ff4d5079bb406ee56cbd4bdecda6573d1c9c128
Gustavo <gustavobritodam@gmail.com>
2020-09-06 17:50:59 -0300

    adicionei o register das views no admin

--- imvwb/admin.py
+++ imvwb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Apartamento, Casa, Bairro
 
 # Register your models here.
+admin.site.register(Apartamento)
+admin.site.register(Casa)
+admin.site.register(Bairro)
