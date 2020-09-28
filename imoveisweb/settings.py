891e1d1afcb2db40fb7f12d092d43554031065ec
samirsaliba <samirtsj@gmail.com>
2020-09-28 12:52:00 -0300

    Set debug to True

--- imoveisweb/settings.py
+++ imoveisweb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-^9qo4o5q8453i&mi3u))mnewct8f%tp3000ir5te1)_23ljct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
